6c4382aca8d09fae2163bd215836227a5b4725d5
Tuval Gutman <tuval.gutman@gmail.com>
2022-04-17 12:34:16 +0300

    add pawns

--- chess.js
+++ chess.js
@@ -1,18 +1,52 @@
-const TableSize=8;
-function addTable()
-{
-    let table =document.createElement('table');
-    let tbody =document.createElement('tbody');
+const TableSize = 8;
+function addTable() {
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
     table.appendChild(tbody);
     document.body.appendChild(table);
-    for(let i=0;i<TableSize;i++)
-    {
-        let row=document.createElement('tr');
-            for(let j=0;j<TableSize;j++)
-            {
-            let square=document.createElement('td');
-            row.appendChild(square);
+    let color;
+    let tool;
+    for (let i = 1; i <= TableSize; i++) {
+        let row = document.createElement('tr');
+
+        switch (i) {
+            case 1: color = ("black");
+                break;
+            case 2: color = ("black");
+                break;
+            default: color = ("white");
+        }
+        for (let j = 1; j <= TableSize; j++) {
+            if (i !== 1 || i !== 7) {
+                switch (j) {
+                    case 1: tool = ("rook");
+                        break;
+                    case 2: tool = ("knight");
+                        break;
+                    case 3: tool = ("bishop");
+                        break;
+                    case 4: tool = ("queen");
+                        break;
+                    case 5: tool = ("king");
+                        break;
+                    case 6: tool = ("bishop");
+                        break;
+                    case 7: tool = ("knight");
+                        break;
+                    case 8: tool = ("rook");
+                        break;
+                }
             }
+            else
+            {
+                tool="pawn";
+            }
+                let square = document.createElement('td');
+            row.appendChild(square);
+            let toolImg=document.createElement('img');
+            toolImg.src='images/'+color+'_'+tool+'.png';
+            square.appendChild(toolImg);
+        }
         tbody.appendChild(row);
     }
 }
