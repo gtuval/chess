3add0b54fa84f32fe6925666b3b587fec396976b
Tuval Gutman <tuval.gutman@gmail.com>
2022-04-17 11:53:57 +0300

    add images

--- chess.js
+++ chess.js
@@ -1,13 +1,14 @@
+const TableSize=8;
 function addTable()
 {
     let table =document.createElement('table');
     let tbody =document.createElement('tbody');
     table.appendChild(tbody);
     document.body.appendChild(table);
-    for(let i=0;i<8;i++)
+    for(let i=0;i<TableSize;i++)
     {
         let row=document.createElement('tr');
-            for(let j=0;j<8;j++)
+            for(let j=0;j<TableSize;j++)
             {
             let square=document.createElement('td');
             row.appendChild(square);
